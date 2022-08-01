cc9b40ca43608a5dd994c29965404f6fd6c76dc5
OlegBodio <bodiosik@gmail.com>
2022-08-01 22:36:06 +0300

    ...

--- source/main/main.cpp
+++ source/main/main.cpp
@@ -1,10 +1,12 @@
-
 #include <pybind11/embed.h>
 #include "section.hpp"
-#include <stdio.h>
+#include "Python.h"
 
+namespace py = pybind11;
 
 int main ()
 {
+    py::scoped_interpreter python;
+    auto sectionPy = py::module_::import("main.py");
     return 0;
 }
