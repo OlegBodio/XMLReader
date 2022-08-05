35a326ccdc57c45a1ebb5daf14fbe47709301c90
OlegBodio <bodiosik@gmail.com>
2022-08-05 18:29:28 +0300

    vector binded

--- source/module/SectionPy.cpp
+++ source/module/SectionPy.cpp
@@ -1,5 +1,8 @@
 #include <pybind11/pybind11.h>
+#include<pybind11/stl_bind.h>
 #include "SectionPy.hpp"
+
+PYBIND11_MAKE_OPAQUE(std::vector<std::string>);
 
 SectionPy::SectionPy(const std::string& name, const std::string& value)
 	:m_root(name, value) 
@@ -12,7 +15,7 @@ Section SectionPy::getSection() const { return m_root; }
 
 void SectionPy::addChild(const SectionPy& child) 
 { 
-	m_root.addChild(getSection()); 
+	m_root.addChild(child.getSection()); 
 }
 
 void SectionPy::setValue(const std::string& value) { m_root.setValue(value);}
@@ -28,11 +31,14 @@ void SectionPy::operator=(const SectionPy& other)
 	m_root = other.m_root;
 }
 
-//PYBIND11_MAKE_OPAQUE(std::string);
-//PYBIND11_MAKE_OPAQUE(std::vector<std::string>);
+
+
+
 
 PYBIND11_MODULE(section_py_module, module)
 {
+	pybind11::bind_vector<std::vector<std::string>>(module, "VectorStr");
+
 	pybind11::class_<SectionPy>(module, "SectionPy")
 		.def(pybind11::init<>())
 		.def(pybind11::init<const std::string&, const std::string&>(), "constructor 2", pybind11::arg("name"), pybind11::arg("value") = "")
--- source/py_scripts/main.py
+++ source/py_scripts/main.py
@@ -1,5 +1,7 @@
 from section_py_module import SectionPy
 
 section = SectionPy('root')
+childSection = SectionPy("child")
+section.addChild(childSection)
 section.setValue('test')
 section.write('test.xml')
